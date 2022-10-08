7efe84b955a9c722cc0df7de6f120361408ab0b4
ellisvelandia <eyis619@gmail.com>
2022-10-07 21:50:18 -0500

    practice

---
+++ app.js
@@ -0,0 +1,32 @@
+// const numberOne = document.getElementById("n1");
+// const numbertwo = document.getElementById("n2");
+// const resp = document.getElementById("resps");
+// const btncalculate = document.getElementById("calculate");
+
+// btncalculate.addEventListener("click", calculate);
+
+// function calculate() {
+//   const op1 = parseFloat(numberOne.value);
+//   const op2 = parseFloat(numbertwo.value);
+
+//   let resps = op1 + op2;
+
+//   resp.innerText = resps;
+
+//   resp.style = "color:red";
+// }
+
+
+// Array
+// const reply = document.getElementById("resp");
+// const btnRunner = document.getElementById("run");
+
+// btnRunner.addEventListener("click", start);
+
+// let fruits = ["apple", "mango", "strawberry", "pear"];
+
+// function start() {
+//   fruits.forEach(function (item, index, array) {
+//     reply.innerHTML = array;
+//   });
+// }
